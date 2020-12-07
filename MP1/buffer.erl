499121027132a2d5f60e989eb07f09a73283879f
Miguel Cardoso <msdc.1000@hotmail.com>
2020-12-07 18:45:00 +0000

    Comments added.

--- MP1/buffer.erl
+++ MP1/buffer.erl
@@ -1,18 +1,23 @@
 -module(buffer).
-
 -export([start/1,bounded_buffer/1]).
 
 start(Size) ->
     register(bounded_buffer, spawn(?MODULE,bounded_buffer,[Size])).
 
 
-%Right now is a LIFO buffer.
+%LIFO buffer.
+%Buffer starting point.
 bounded_buffer(Size)->
-    io:format("Buffer was initialised!~n"),
+    io:format("Buffer was initialised, with size ~p!~n",[Size]),
     loop(Size,[]).
 
 loop(Size,Values)-> 
+    
+    %Blocks until it receives something from the message queue
     receive {produce,Value,Client}
+      %If this process receives this pattern and the Value is a number, tries to add it to the list if not full.
+      %Sends back to the producer {reply,Value} if success
+      %Sends back to the producer full if the value could not be added because buffer was full.
       when is_number(Value) ->  
         if length(Values) < Size ->
             io:format("Added ~p~n",[Value]), Client ! {reply,Value},loop(Size,[Value | Values]);
@@ -20,15 +25,22 @@ loop(Size,Values)->
             io:format("Buffer is full!~n"),Client ! full, loop(Size,Values)
         end;
 
+      %If this process receives this pattern, it tries to remove the tail value of the list and return it to the consumer.
+      %Sends back to the consumer {reply,Value} if success
+      %Sends back to the producer empty if the value could not be retrieved because the buffer was empty.
       {consume,Client} -> 
           if length(Values) == 0 ->
                 io:format("Buffer is empty!~n"), Client! empty, loop(Size,Values);
         true ->
-          io:format("Removed ~p~n",[hd(Values)]),Client !  {reply,hd(Values)} , loop(Size,tl(Values))
+          REV = lists:reverse(Values),
+          REMOVED = hd(REV),
+          io:format("Removed ~p~n",[REMOVED]),Client !  {reply,REMOVED} , loop(Size,lists:reverse(tl(REV)))
         end;
-    
+        
+      %Prints the elements in the buffer.
       read -> io:format("All Elements ~p~n",[Values]),loop(Size,Values);
     
+      %Unknown message, discards it.
       Msg -> io:format("Message discarded: ~p~n", [Msg]), loop(Size,Values)
 
     end.
--- MP1/monitor.erl
+++ MP1/monitor.erl
@@ -1,27 +1,29 @@
 -module(monitor).
 
--export([init/0,monitor_init/0]).
+-export([init/1,monitor_init/1]).
 
 -import(buffer,[start/1]).
-%-import(compile,[c/1]).
 
-init() ->
+
+init(Size) ->
     
-   spawn(?MODULE,monitor_init,[]).
+   spawn(?MODULE,monitor_init,[Size]).
    
 
 
-monitor_init() -> 
+monitor_init(Size) -> 
 	
 	io:format("Starting Bounded Buffer...~n"),
-	buffer:start(5),
+    %Starts buffer with size Size.
+	buffer:start(Size),
 
+    %Makes this processa a monitor
 	monitor(process, bounded_buffer),
 	io:format("Started Monitoring Bounded Buffer...~n"),
 
 	receive
     {'DOWN', _Ref, process, Pid, Why} ->
-        io:format("Bounded Buffer died because ~p~n",[Why]),
-        % add the restart logic
-        monitor_init()
+        io:format("~p Bounded Buffer died because ~p~n",[Pid,Why]),
+        % Restart buffer.
+        monitor_init(Size)
     end.
